0481854ce788574fd97b5367b31ccab2279c02ac
JlaraCast <163551527+JlaraCast@users.noreply.github.com>
2026-07-19 16:55:10 -0600

    Update footer.jsx

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -26,6 +26,7 @@ function Footer() {
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about">About us</Link></li>
             <li><Link to="/services">Services</Link></li>
+            <li><Link to="/portfolio">Portfolio</Link></li>
             <li><Link to="/contact">Contáctanos</Link></li>
           </ul>
         </div>
